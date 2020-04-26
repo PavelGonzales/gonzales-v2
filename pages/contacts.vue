264cd1300c6e1b3aaa4ce2d15f8c23b9f580deca
Павел Гонзалес <p.gonzales@gonzales-p-msk.local>
2020-04-27 01:29:54 +0300

    🧱 Add contacts page

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,6 +1,42 @@
 <template>
-  <div class="title">
-    Contacts
+  <div :class="$style.container">
+    <ul :class="$style.socialList">
+      <li :class="$style.socialItem">
+        <a href="mailto:develop.gonzales@gmail.com" target="_blank">
+          <img src="./../assets/icons/google.svg" alt="gmail" :class="$style.socialImg">
+        </a>
+      </li>
+      <li :class="$style.socialItem">
+        <a href="https://www.linkedin.com/in/pavelgonzales/" target="_blank">
+          <img src="./../assets/icons/linkedin.svg" alt="linkedin" :class="$style.socialImg">
+        </a>
+      </li>
+      <li :class="$style.socialItem">
+        <a href="https://www.facebook.com/develop.gonzales" target="_blank">
+          <img src="./../assets/icons/facebook.svg" alt="facebook" :class="$style.socialImg">
+        </a>
+      </li>
+      <li :class="$style.socialItem">
+        <a href="https://www.instagram.com/pavelgonzales/" target="_blank">
+          <img src="./../assets/icons/instagram.svg" alt="instagram" :class="$style.socialImg">
+        </a>
+      </li>
+      <li :class="$style.socialItem">
+        <a href="https://medium.com/@pavelgonzales" target="_blank">
+          <img src="./../assets/icons/medium.svg" alt="medium" :class="$style.socialImg">
+        </a>
+      </li>
+      <li :class="$style.socialItem">
+        <a href="https://github.com/PavelGonzales" target="_blank">
+          <img src="./../assets/icons/github.svg" alt="github" :class="$style.socialImg">
+        </a>
+      </li>
+      <li :class="$style.socialItem">
+        <a href="tg://resolve?domain=pavelgonzales" target="_blank">
+          <img src="./../assets/icons/telegram.svg" alt="telegram" :class="$style.socialImg">
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,14 +46,38 @@ export default {
 }
 </script>
 
-<style scoped>
-  .title {
-    font-size: 70px;
-    font-weight: 600;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #fff;
-    width: 100%;
+<style module>
+@import '~/assets/styles/main.css';
+
+.socialList {
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.socialItem {
+  width: 10.5%;
+}
+
+.socialImg {
+  width: 100%;
+}
+
+@media (max-width: 640px) {
+  .socialList {
+    justify-content: flex-start;
   }
+
+  .socialItem {
+    width: 60px;
+    margin-bottom: 12%;
+    margin-right: 12%;
+  }
+
+  .socialItem:nth-child(4n + 4) {
+    margin-right: 0;
+  }
+}
 </style>
